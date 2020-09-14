fa2fa90ce70845721ec7815e5eae9df5bf19c399
ad255500 <asherison@gmail.com>
2020-09-14 19:03:50 -0400

    added src files

--- Cornelison_p1/src/Decrypter.java
+++ Cornelison_p1/src/Decrypter.java
@@ -1,2 +1,38 @@
 public class Decrypter {
+    public String decrypt(String input) {
+        int[] d = new int[4];
+        d = toInt(input);
+        d = math(d);
+        return toString(d);
+    }
+
+    public static int[] toInt(String s) {
+        int[] fromInt = new int[4];
+        for (int i = 0; i < 4; i++) {
+            fromInt[i] = Integer.parseInt(String.valueOf(s.charAt(i)));
+        }
+        return fromInt;
+    }
+
+    public static int[] math(int[] in) {
+        int[] out = new int[4];
+        out[1] = in[3];
+        out[0] = in[2];
+        out[3] = in[1];
+        out[2] = in[0];
+        for (int i = 0; i < 4; i++) {
+            out[i] -= 7;
+            out[i] += 10;
+            out[i] %= 10;
+        }
+        return out;
+    }
+
+    public static String toString(int[] s) {
+        String out = "";
+        for (int i = 0; i < 4; i++) {
+            out += s[i];
+        }
+        return out;
+    }
 }
--- Cornelison_p1/src/Encrypter.java
+++ Cornelison_p1/src/Encrypter.java
@@ -1,3 +1,38 @@
 public class Encrypter {
-    
+
+    public String encrypt(String input) {
+        int[] e = new int[4];
+        e = toInt(input);
+        e = math(e);
+        return toString(e);
+    }
+
+    public static int[] toInt(String s) {
+        int[] fromInt = new int[4];
+        for (int i = 0; i < 4; i++) {
+            fromInt[i] = Integer.parseInt(String.valueOf(s.charAt(i)));
+        }
+        return fromInt;
+    }
+
+    public static int[] math(int[] in) {
+        int[] out = new int[4];
+        for (int i = 0; i < 4; i++) {
+            in[i] += 7;
+            in[i] %= 10;
+        }
+        out[0] = in[2];
+        out[1] = in[3];
+        out[2] = in[0];
+        out[3] = in[1];
+        return out;
+    }
+
+    public static String toString(int[] s) {
+        String out = "";
+        for (int i = 0; i < 4; i++) {
+            out += s[i];
+        }
+        return out;
+    }
 }
